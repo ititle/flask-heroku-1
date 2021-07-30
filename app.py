665fb5005be286b8de5022c5c9e523394ce89648
ititle <87840877+ititle@users.noreply.github.com>
2021-07-30 10:08:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,11 @@ def hello():
 def get_api():
     return jsonify(data)
 
+
+@app.route('/name')
+def name():
+  return"กฤศ วงษ์เหม เลขที่1 ชั้น ม.4/10"       
+
+
 if __name__ == "__main__":
     app.run(debug=False)
